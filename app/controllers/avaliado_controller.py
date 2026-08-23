b0777463f27e985012682c2af8fa923b8c5ac5bc
Levyatam696 <joselevy115@gmail.com>
2026-08-22 23:58:40 -0300

    Implementa classe de avaliado

--- app/controllers/avaliado_controller.py
+++ app/controllers/avaliado_controller.py
@@ -7,14 +7,42 @@
 #O sistema deve calcular a idade automaticamente
 #O sistema deve armazenar corretamente os dados
 #O sistema deve permitir edição posterior
-
 from dataclasses import dataclass
-from datetime import date
+import datetime
 
 
 @dataclass
 class Avaliado:
-	nome: str
-	sexo: str
-	data_nascimento: date
+    nome: str
+    sexo: str 
+    data_nascimento: datetime.date
+
+    def calcular_idade(self):
+        hoje = datetime.date.today()
+        idade = hoje.year - self.data_nascimento.year - ((hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day))
+        return idade
+
+
+    def validar_idade(self):
+        idade = self.calcular_idade()
+        if 4 < idade < 80:
+            return True
+        else:
+            raise ValueError("Idade inválida. Deve estar entre 5 e 79 anos.")
+
+
+    def __post_init__(self):
+        if not self.nome.replace(" ", "").isalpha():
+            raise ValueError("Nome deve conter apenas letras e espaços.")
+
+
+        if self.sexo not in ["Masculino", "Feminino", "Outro"]:
+            raise ValueError("Sexo inválido. Opções válidas: Masculino, Feminino, Outro.")
+
+        
+        if self.data_nascimento > datetime.date.today():
+            raise ValueError("Data de nascimento não pode ser futura.")
+
+        self.validar_idade()
+
 
