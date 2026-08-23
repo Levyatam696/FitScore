75ba24c620b28497502e534de416067d718702d5
Levyatam696 <joselevy115@gmail.com>
2026-08-22 23:56:58 -0300

    Implementa classe de avaliado

--- app/controllers/avaliado_controller.py
+++ app/controllers/avaliado_controller.py
@@ -1,1 +1,20 @@
-﻿
+﻿#O sistema deve permitir inserir nome, sexo e data de nascimento
+#Todos os campos devem ser obrigatórios
+#O sistema deve validar:
+#Nome: apenas letras e espaços
+#Sexo: opções válidas
+#Data: formato válido e não futura
+#O sistema deve calcular a idade automaticamente
+#O sistema deve armazenar corretamente os dados
+#O sistema deve permitir edição posterior
+
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class Avaliado:
+	nome: str
+	sexo: str
+	data_nascimento: date
+
